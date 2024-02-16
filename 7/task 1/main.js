37bc1bb8002ff258bf71fd9d0c917294d84eb5fb
Anniq26 <aniqitoshvili2012@gmail.com>
2024-02-17 01:11:02 +0400

    Update html

--- 7/task 1/main.js
+++ 7/task 1/main.js
@@ -1,38 +1,11 @@
-const modal = document.getElementById("myModal");
-const btn = document.getElementById("openModalBtn");
-const span = document.getElementsByClassName("close")[0];
-const button = document.getElementById("submitBtn");
-const form = document.querySelector('.form');
-
-btn.onclick = (() => {
-    modal.style.display = "block";
-  })
-  
-  span.onclick =  (() => {
-    modal.style.display = "none";
-  })
-
-  window.onclick =  (() => {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  })
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-
-  const input = document.querySelector('input[name="colorInput"]')
-
-  if(!input) {
-    alert('Please enter the color');
-    return
-  }
-
-  if(input === 'red' || input === 'blue', input === 'green', input === 'black', input === 'white'){
-    modal.style.background = Input;} 
-    
-    if (input !== 'red' || input !== 'blue', input !== 'green', input !== 'black', input !== 'white'){
-  alert("Please enter 'red', 'green', or 'blue'")
+function delay(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
 }
- 
-})
+
+// Example usage:
+console.log('Start');
+delay(2000).then(() => {
+  console.log('This message will be delayed by 2 seconds');
+});
