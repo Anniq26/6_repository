7a63290ca83287562c2f8357d3be23134c4802b5
Anniq26 <aniqitoshvili2012@gmail.com>
2024-03-26 00:20:00 +0400

    add task

--- 18/my-app/src/App.jsx
+++ 18/my-app/src/App.jsx
@@ -6,33 +6,93 @@ const API_KEY = 'YZj-nd9Tf1je3ZQGi1z6T1WErm0qmhdOXiY2s9epUimbHsDRSw'
 
 function App() {
   const[taskList, setTaskList] = useState([])
+  const getTasks = () => {
+    fetch('/api/v1/tasks',{
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${API_KEY}`
+      }
+    }).then(res => { 
+    if (!res.ok) throw new Error("Respomse failed");
+    return res.json()
+  })
+  .then(data => setTaskList((prev) => [...prev, {
+    taskName: data.items[0].taskName,
+    id: data.items[0]._uuid
+  }]))
+  .catch(err => console.log(err))
+  }
+  const addTask = (taskName, isCompleted) => {
+        fetch('/api/v1/tasks', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${API_KEY}`
+            },
+            body: JSON.stringify([{ name: taskName, isCompleted }])
+        })
+        .then(res => {
+            if (!res.ok) throw new Error("Failed to add task");
+            return res.json();
+        })
+        .then(data => {
+            setTaskList(prevList => [...prevList, data.items[0]]);
+        })
+        .catch(err => console.error(err));
+    };
 
   const onFormSubmit = (taskName) => {
-    fetch ('/api/v1/task', { 
-      method: "POST" ,
+    fetch ('/api/v1/tasks', { 
+      method: "POST",
       headers: {
           "Content-Type": "application/json",
           "Authorization": `Bearer ${API_KEY}`
       },
-      body: JSON.stringify([{ "title": taskName, "completed": false }]) 
+      body: JSON.stringify([{taskName}])
 })
 .then(res => {
-    if (!res.ok) throw new Error("Failed to submit task");
-    return res.json();
+    if (!res.ok) throw new Error("Respomse failed");
+    return res.json()
 })
 .then(data => setTaskList((prev) => [...prev, {
           taskName: data.items[0].taskName,
           id: data.items[0]._uuid
         }]))
-        .catch(err => console.log(err)) 
+        .catch(err => console.log(err))
+        const addTask = (taskName, isCompleted) => {
+          fetch('/api/v1/tasks', {
+              method: "POST",
+              headers: {
+                  "Content-Type": "application/json",
+                  "Authorization": `Bearer ${API_KEY}`
+              },
+              body: JSON.stringify([{ name: taskName, isCompleted }])
+          })
+          .then(res => {
+              if (!res.ok) throw new Error("Failed to add task");
+              return res.json();
+          })
+          .then(data => {
+              setTaskList(prevList => [...prevList, data.items[0]]);
+          })
+          .catch(err => console.error(err));
+      }; 
           }
+          
   return (
     <div className="App">
       <TaskForm onFormSubmit={onFormSubmit}/>
 
-      {taskList.map((task) => <div key={task.id}>
+      {taskList.map((task) =>( <div key={task.id}>
         <h3>{task.taskName}</h3>
-      </div>)}
+        <button onClick={getTasks}>get tasks</button>
+      </div>))}
+      {taskList.map(task => (
+                    <li key={task._uuid}>
+                        {task.name} - Completed: {task.isCompleted ? "Yes" : "No"}
+                    </li>
+                ))}
     </div>
   );
 }
--- 18/my-app/src/components/TaskForm.jsx
+++ 18/my-app/src/components/TaskForm.jsx
@@ -1,22 +1,30 @@
+import React from "react";
 import { useState } from "react"
 
 const TaskForm = ({onFormSubmit}) => {
 
     const [taskName, setTaskname] = useState()
+    const [isCompleted, setIsCompleted] = useState(false);
 
 
     const onSubmit = (e) => {
         e.preventDefault()
-        onFormSubmit(taskName)
-    }
+        onFormSubmit(taskName, isCompleted)
+    };
 
     return <form onSubmit={onSubmit}>
         <input type="text" 
         placeholder="TaskName" 
+        value={taskName}
         onChange={e => setTaskname(e.target.value)}
         />
-
+        
         <button>Submit</button>
+        <input
+                    type="checkbox"
+                    checked={isCompleted}
+                    onChange={(e) => setIsCompleted(e.target.checked)}
+                />
     </form>
 }
 
